6944113bc2950a2b1239bf1cce4add77cc5c9a06
CristianDavideConte <64142998+CristianDavideConte@users.noreply.github.com>
2020-05-22 20:43:50 +0200

    cleaned up code

--- js/website.js
+++ js/website.js
@@ -1,15 +1,18 @@
 var windowInnerWidth;															//A shortcut for the DOM element window.innerWidth
 var windowInnerHeight;															//A shortcut for the DOM element window.innerHeight
-var windowPosition;																//The scrollTop value of the window, used to prevent the DOM from scrolling after a screen orientation change 						
+//var windowPosition;																//The scrollTop value of the window, used to prevent the DOM from scrolling after a screen orientation change 						
 var documentBodyElement;														//A shortcut for the HTML element document.body 
-var headerElement;																//The HTML element with the id "header", used as the website navbar 
-var hamburgerMenuElement;														//The HTML element with the	id "hamburgerMenu", used to interact with the navbar when the width of the window is below 1081px 								
-var pageLinksElements; 															//All HTML element with the class "pageLink", shown in the header to navigate through the website' sections
-var contentElement;																//The HTML element with the id "content", used as the website main container
 var currentPageIndex;															//The index of the HTML element with class "page" that is currently being displayed the most: if the page is 50% or on the screen, than it's currently being displayed
-var websitePreviewExpandedMap; 													//A map which contains all the already expanded websitePreviews HTML elements, used for not having to recalculate them every time the user wants to see them
 var transitionTimeMedium;														//The --transition-time-medium css variable, used to know the duration of the normal speed-transitioning elements
 var mobileMode; 																//Indicates if the css for mobile is currently beign applied
+var headerElement;																//The HTML element with the id "header", used as the website navbar 
+var hamburgerMenuElement;														//The HTML element with the id "hamburgerMenu", used to interact with the navbar when the width of the window is below 1081px 								
+var pageLinksElements; 															//All HTML elements with the class "pageLink", shown in the header to navigate through the website' sections
+var contentElement;																//The HTML element with the id "content", used as the website main container
+var carouselButtons;															//All HTML elements with the class "carouselButton", used to scroll the websitePreview carousel
+var websiteShowcase;															//The HTML element with the id "websiteShowcase", children of the websitePreviewCarousel HTML element and used as container for all the websitePreviews
+var websitePreviews;															//All HTML elements with the class "websitePreview", used as a clickable previews for all the projects inside the websitePreviewShowcase 
+var websitePreviewExpandedMap; 													//A map which contains all the already expanded websitePreviews HTML elements, used for not having to recalculate them every time the user wants to see them
 
 /* This Function calls all the necessary functions that are needed to initialize the page */
 function init() {	
@@ -22,7 +25,7 @@ function init() {
 	//setTimeout(() => scrollTest(directionScroll), 5000);
 }
 
-/* This Function initializes all the javascript file's public variables */
+/* This Function initializes all the public variables */
 function variableInitialization() {
 	documentBodyElement = document.body;
 	
@@ -30,6 +33,9 @@ function variableInitialization() {
 	hamburgerMenuElement = document.getElementById("hamburgerMenu");	
 	pageLinksElements = document.getElementsByClassName("pageLink");	
 	contentElement = document.getElementById("content");
+	carouselButtons = document.getElementsByClassName("carouselButton");	
+	websiteShowcase = document.getElementById("websiteShowcase");
+	websitePreviews = document.getElementsByClassName("websitePreview");
 	
 	transitionTimeMedium = getComputedStyle(documentBodyElement).getPropertyValue("--transition-time-medium").replace("s", "") * 1000;
 	websitePreviewExpandedMap = new Map();
@@ -38,6 +44,9 @@ function variableInitialization() {
 /* This function binds all the HTML elements that can be interacted to their mouse and keyboard eventHandlers */
 function desktopEventListenerInitialization() {
 	window.addEventListener("resize", updateWindowSize, {passive:true});										//Updates the height and the width whenever the window's resized
+	/* The user can use the arrow keys to navigate the website.
+	 * Controls for the original event keydown target are added to avoid accidental page scroll.
+	 */
 	documentBodyElement.addEventListener("keydown", event => {
 		if(event.target.tagName == "BODY") {
 			let keyName = event.key;
@@ -51,23 +60,23 @@ function desktopEventListenerInitialization() {
 		}
 	}, {passive:false});
 
-	headerElement.addEventListener("wheel", event => event.stopPropagation(), {passive:false});			
+	headerElement.addEventListener("wheel", event => event.stopPropagation(), {passive:true});					//Scroll on the header is not allowed		
 	hamburgerMenuElement.addEventListener("click", toggleExpandHamburgerMenu, {passive:true});					//When the hamburgerMenu is pressed it expands by calling the toggleExpandHamburgerMenu function 
 	
+	/* When the website is in mobile mode the page links are hidden under the hamburgerMenu 
+	 * which can be expanded by toggling the class "mobileExpanded" on the header.
+	 * Once it's expanded the pageLinks can be clicked to go to the relative website section.
+	 * Whenever a link is clicked and the contentElement is scrolled, it's convenient to hide all the links under the hamburgerMenu.
+	 * This is done the same way the hamburgerMenu expands when clicked directly (see above in the comment).
+	 */
 	for(const pageLinkElement of pageLinksElements)															
-		pageLinkElement.addEventListener("click", toggleExpandHamburgerMenu, {passive:true});				//Whenever a HTML element with the class "pageLink" is pressed the DOM is scrolled to the corresponding section 						
+		pageLinkElement.addEventListener("click", toggleExpandHamburgerMenu, {passive:true});											
 	
-	let githubContactElement = document.getElementById("githubContact");
-	githubContactElement.addEventListener("click", () => window.open("https://github.com/CristianDavideConte"), {passive:true});
-	
-	let instagramContactElement = document.getElementById("instagramContact");
-	instagramContactElement.addEventListener("click", () => window.open("https://www.instagram.com/cristiandavideconte/?hl=it"), {passive:true});
-		
-	let facebookContactElement = document.getElementById("facebookContact");
-	facebookContactElement.addEventListener("click", () => window.open("https://www.facebook.com/cristiandavide.conte/"), {passive:true});		
-	
-	let mailContactElement = document.getElementById("mailContact");
-	mailContactElement.addEventListener("click", () => window.open("mailto:cristiandavideconte@gmail.com", "mail"), {passive:true});
+	/* All the */
+	document.getElementById("githubContact").addEventListener("click", () => window.open("https://github.com/CristianDavideConte"), {passive:true});
+	document.getElementById("instagramContact").addEventListener("click", () => window.open("https://www.instagram.com/cristiandavideconte/?hl=it"), {passive:true});
+	document.getElementById("facebookContact").addEventListener("click", () => window.open("https://www.facebook.com/cristiandavide.conte/"), {passive:true});		
+	document.getElementById("mailContact").addEventListener("click", () => window.open("mailto:cristiandavideconte@gmail.com", "mail"), {passive:true});
 	
 	let firstScrollYPosition = null;
 	let lastScrollYPosition; 	
@@ -85,17 +94,16 @@ function desktopEventListenerInitialization() {
 			}, 100);
 	}, {passive:true});
 
-	let websiteShowcase = document.getElementsByClassName("websiteShowcase")[0];
 	websiteShowcase.addEventListener("wheel", (event) => {
+		let scrollDirection = Math.sign(event.deltaY);					//1 if the scrolling is going downwards -1 otherwise
+		let totalScrollAmmount = windowInnerWidth/20;					//The total ammount of pixel horizontally scrolled by the smoothScroll function 
+		let scrollDistance = windowInnerWidth/150;						//The ammount of pixel scrolled at each smoothScroll call
+		let partialScrollAmmount = 0;									//scrollDistance * number of smoothScroll function calls
+		
 		/* The number of the pixel scrolled on the x-axis, it's calculated dynamically based on the windowInnerWidth 
 		 * and so that is 1/20th of the window's innerWidth at any given resolution.
-		 * If the wheel is scrolled from top to bottom the scroll direction will be from right to left, will be inverted otherwise.
+		 * If the wheel is scrolled from top to bottom the scroll direction will be from right to left, it will be inverted otherwise.
 		 */
-		let scrollDirection = Math.sign(event.deltaY);
-		let totalScrollAmmount = windowInnerWidth/20;
-		let scrollDistance = windowInnerWidth/150;
-		let partialScrollAmmount = 0;
-		
 		function smoothScroll() {
 			websiteShowcase.scrollLeft += scrollDirection * scrollDistance;		
 			partialScrollAmmount += scrollDistance;
@@ -105,8 +113,7 @@ function desktopEventListenerInitialization() {
 		
 		window.requestAnimationFrame(smoothScroll);
 	}, {passive:true});
-	
-	let carouselButtons = document.getElementsByClassName("carouselButton");														
+														
 	let carouselButtonScrollEnabled = false;
 	/* The number of the pixel scrolled on the x-axis, it's calculated dynamically based on the windowInnerWidth 
 	 * and so that is +1/100th of the window's innerWidth at any given resolution.
@@ -141,7 +148,6 @@ function desktopEventListenerInitialization() {
 	carouselButtons[0].addEventListener("mouseup", () => carouselButtonScrollEnabled = false, {passive:true});
 	carouselButtons[1].addEventListener("mouseup", () => carouselButtonScrollEnabled = false, {passive:true});
 	
-	websitePreviews = document.getElementsByClassName("websitePreview");
 	for(const websitePreview of websitePreviews) {
 		/* First all the websitePreviewExpanded basic components are created */
 		let websitePreviewExpanded = document.createElement("div");
@@ -187,7 +193,7 @@ function desktopEventListenerInitialization() {
 		backgroundContent.className = "page";		
 		backgroundContent.appendChild(websitePreviewExpanded);
 		
-		/*This variable is used to prevent the user to execute the backgroundContent eventListener 
+		/*The listenersAlreadyTriggered variable is used to prevent the user to execute the backgroundContent eventListener 
 		 * more than one time while the animation is still happening.
 		 * Otherwise the document.body would try to remove the backgroundContent multiple times generating errors in the browser console.
 		 * Note that this bug wouldn't cause the page to instantly crash.
@@ -314,7 +320,7 @@ function toggleExpandHamburgerMenu() {
  * It triggers after a scroll is completed.
  * If at the end of the scroll, the current page is not alligned, it gets:
  * - alligned if it covers 3/4 of the windowInnerHeight or more
- * - scrolled following the original user's scroll direction, otherwise 
+ * - scrolled, following the original user's scroll direction, otherwise 
  */
 function smoothPageScroll(direction, contentElementScrollTop) {
 	currentPageIndex = Math.round(contentElementScrollTop / windowInnerHeight);
