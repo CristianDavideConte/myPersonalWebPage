747355b461482dc8d31efdd2f8ccb2dccfd6ce3d
CristianDavideConte <64142998+CristianDavideConte@users.noreply.github.com>
2020-08-28 14:31:29 +0200

    Update tests.js

--- js/tests.js
+++ js/tests.js
@@ -1,27 +1,27 @@
 //Returns true if the browser used is Apple's Safari, false otherwisee
-let safariBrowserUsed;
+let _safariBrowserUsed;
 function browserIsSafari() {
-	safariBrowserUsed = navigator.vendor && navigator.vendor.indexOf("Apple") > -1 &&
+	_safariBrowserUsed = navigator.vendor && navigator.vendor.indexOf("Apple") > -1 &&
 									   	navigator.userAgent &&
 									   	navigator.userAgent.indexOf("CriOS") == -1 &&
 									   	navigator.userAgent.indexOf("FxiOS") == -1;
-	return safariBrowserUsed;
+	return _safariBrowserUsed;
 }
 
 //Performs a stress test for the headerElement and the headerBackgroundElement
 var _stressTestHeaderCounter = 0;
-function _stressTestHeader() {
+function stressTestHeader() {
 	var _testEvent = document.createEvent('Events');
 	_testEvent.initEvent("click", true, false);
 	if(_stressTestHeaderCounter < 100) {
 		hamburgerMenuElement.dispatchEvent(_testEvent);
-		window.setTimeout(_stressTestHeader, transitionTimeMedium);
+		window.setTimeout(stressTestHeader, transitionTimeMedium);
 		_stressTestHeaderCounter++;
 	}
 }
 
 var _stressTestWebsitePreviewCounter = 0;
-function _stressTestWebsitePreview() {
+function stressTestWebsitePreview() {
   let _testEvent = document.createEvent('Events');
   _testEvent.initEvent("click", true, false);
 	if(_stressTestWebsitePreviewCounter < 100) {
@@ -30,27 +30,27 @@ function _stressTestWebsitePreview() {
 		else
 			websitePreviewExpandedBackgroundContentElement.dispatchEvent(_testEvent);
 
-		window.setTimeout(_stressTestWebsitePreview, transitionTimeMedium + 100);
+		window.setTimeout(stressTestWebsitePreview, transitionTimeMedium + 100);
 		_stressTestWebsitePreviewCounter++;
 	}
 }
 
 //Performs a very fast (resource intensive) scroll test
 var _stressTestVeryFastScrollCounter = 0;
-function _stressTestVeryFastScroll() {
+function stressTestVeryFastScroll() {
 	if(_stressTestVeryFastScrollCounter < 100){
 		let _scrollAmmount = (_stressTestVeryFastScrollCounter % 2 === 0) ? windowHeight / 4 : -windowHeight / 4;
-		windowScrollYBy(_scrollAmmount, _stressTestVeryFastScroll);
+		windowScrollYBy(_scrollAmmount, stressTestVeryFastScroll);
 		_stressTestVeryFastScrollCounter++;
 	}
 }
 
 //Performs a scrolling test scrolling the whole document
 var _stressTestFullDocumentScrollCounter = 0;
-function _stressTestFullDocumentScroll() {
+function stressTestFullDocumentScroll() {
 	if(_stressTestFullDocumentScrollCounter < 20) {
 		let _scrollAmmount = (_stressTestFullDocumentScrollCounter % 2 === 0) ? windowHeight * 4 : -windowHeight * 4;
-		windowScrollYBy(_scrollAmmount, _stressTestFullDocumentScroll);
+		windowScrollYBy(_scrollAmmount, stressTestFullDocumentScroll);
 		_stressTestFullDocumentScrollCounter++;
 	}
 }
